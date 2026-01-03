548092b96fa843f020540eb45de63254ba5371a0
Alone <amirhoseinsharify90@gmail.com>
2026-01-04 00:26:12 +0330

    and secendry constructor to code ; 2026-01-03

--- main.kt
+++ main.kt
@@ -1,11 +1,13 @@
 fun main() {
     val user1 = User("jlex", "Amir", 23)  // استفاده از سازنده اول
-    val user2 = User("jlex")  // استفاده از سازنده دوم
+    val user2 = User("jlex")  
+    val user3 = User("jlex","mamad") 
 }
 
 class User(var name: String, var lastname: String, var age: Int) {
 
     // سازنده دوم که فقط نام می‌گیرد و بقیه مقادیر به صورت پیش‌فرض تنظیم می‌شوند
     constructor(name: String): this(name, "User", 0)
+    constructor(name: String,lastname:String): this(name, lastname, 0)
 }
 
