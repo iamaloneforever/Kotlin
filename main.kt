e957965af5ed1b31ae61955992f949dac6cb63ed
Alone <amirhoseinsharify90@gmail.com>
2026-01-04 17:59:50 +0330

    Enhance User class with custom getter and setter for name property; update main function to demonstrate usage and print statements.

--- main.kt
+++ main.kt
@@ -1,11 +1,25 @@
 fun main() {
-    val user1 = User(lastname = "jlex", name = "Amir",age = 23)  // استفاده از سازنده اول
-    val user2 = User("jlex")  
-    val user3 = User("jlex","mamad") 
+    val user1 = User(lastname = "jlex", name = "Amir", age = 23)
+    val user2 = User("jlex")
+    val user3 = User("jlex","mamad")
+
+    println(user1.name)
+    println(user2.name)
+    println(user3.name)
+
+    user1.name = "Ali"  // این باعث می‌شه setter اجرا بشه و پیغام چاپ بشه
 }
 
-class User(var name: String, var lastname: String = "Lastname", var age: Int = 0) {
 
+class User(name: String, var lastname: String = "Lastname", var age: Int = 0) {
+    var name = name
+    get() {
+        return "FirstName : $field"
+    }
+    set(value){
+        println("$value was assigned")
+        field = value
+    }
  
 }
 
