540158b1298f9c1c06407bcdaad46ccf2462d8c6
Alone <amirhoseinsharify90@gmail.com>
2026-01-04 19:46:36 +0330

    OOP - lateinit

--- main.kt
+++ main.kt
@@ -1,24 +1,10 @@
 fun main() {
     val user1 = User(lastname = "jlex", name = "Amir", age = 23)
-    val user2 = User("jlex")
-    val user3 = User("jlex","mamad")
-
-    println(user1.getFirstName())
-    println(user2.name)
-    println(user3.name)
-
-    user1.name = "Ali"  // این باعث می‌شه setter اجرا بشه و پیغام چاپ بشه
 }
 
 
-class User(name: String, var lastname: String = "Lastname", var age: Int = 0) {
-    var name = name
+class User(var name: String, var lastname: String, var age: Int) {
+   lateinit var favoriteMovie: String
 
-    fun setFirstName(newValue:String){
-        this.name = newValue
-    }
-    fun getFirstName():String{
-        return this.name
-    }
 }
 
