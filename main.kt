44965af510b36a8a44a53b28469ffdd52a3103ad
Alone <amirhoseinsharify90@gmail.com>
2026-01-04 17:48:04 +0330

    Refactor User class constructors for improved clarity and default values; update main function to use named parameters.

--- main.kt
+++ main.kt
@@ -1,13 +1,11 @@
 fun main() {
-    val user1 = User("jlex", "Amir", 23)  // استفاده از سازنده اول
+    val user1 = User(lastname = "jlex", name = "Amir",age = 23)  // استفاده از سازنده اول
     val user2 = User("jlex")  
     val user3 = User("jlex","mamad") 
 }
 
-class User(var name: String, var lastname: String, var age: Int) {
+class User(var name: String, var lastname: String = "Lastname", var age: Int = 0) {
 
-    // سازنده دوم که فقط نام می‌گیرد و بقیه مقادیر به صورت پیش‌فرض تنظیم می‌شوند
-    constructor(name: String): this(name, "User", 0)
-    constructor(name: String,lastname:String): this(name, lastname, 0)
+ 
 }
 
