6df8566cf10d89f963ecad0b7a2efab32601c11d
Alone <amirhoseinsharify90@gmail.com>
2026-01-07 13:12:17 +0330

    Abstract classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,18 @@
 fun main(args: Array<String>) {
-    val success = Result.Success("Success")
-    val error = Result.Error("FAILED")
-    val progress = Result.Progress("Progress")
-    getData(progress)
-}
 
-fun getData(result: Result){
-    when (result){
-        is Result.Success -> result.showMessage()
-        is Result.Error -> result.showMessage()
-        is Result.Progress -> result.showMessage()
-    }
 }
+abstract class Vehicle(){
 
-sealed class Result(val message : String){
-    fun showMessage(){
-        println("Result is $message")
+    abstract fun move()
+    abstract fun stop()
+}
+class Car(var name:String,var color:String,val engines:Int,val doors:Int):Vehicle(){
+    override fun move() {
+
     }
-    class Success(message: String) : Result(message)
-    class Error(message: String) : Result(message)
-    class Progress(message: String) : Result(message)
+
+    override fun stop() {
+
+    }
+
 }
